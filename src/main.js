bac9b3cf27ecc7093ca43080a98934b4cfce7769
Memory1031 <1016668619@qq.com>
2019-12-02 15:20:29 +0800

    更新了登录功能

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
+import '@/interceptors'
 
 Vue.use(ViewUI);
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/group/login/login'
+import User from '@/group/user/user'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: {name: 'login'}
+    },
+    {
+      path: '/login',
+      component: Login,
+      name: 'login'
+    },
+    {
+      path: '/user',
+      component: User
     }
   ]
 })
