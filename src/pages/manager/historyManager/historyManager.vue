bbd3887b40504605f7c3715a9323d8d5b6bf88ee
Memory1031 <50623917+Memory1031@users.noreply.github.com>
2019-12-17 22:05:45 +0800

    完成了管理员查看预约历史的功能

---
+++ src/pages/manager/historyManager/historyManager.vue
@@ -0,0 +1,223 @@
+<template>
+  <div>
+    <Row>
+      <div class="button">
+        <Input size="large" placeholder="输入房间号（选填）"
+               style="width: 250px" clearable
+               v-model="roomName"></Input>
+        <Button type="success" class="refresh_button" @click="searchForApply">筛选</Button>
+      </div>
+    </Row>
+    <Row>
+      <Table
+        size="large"
+        no-data-text="当前没有会议室申请"
+        stripe border
+        :loading="loading"
+        height="700"
+        :columns="columns" :data="data"></Table>
+      <Modal v-model="modal_create" width="600" :closable="false">
+        <div>
+          <Card style="margin-bottom: 25px">
+            <p>
+              <span>您的评价：</span>
+              <Tag v-for="item in tagList"
+                   :key="item.id" :name="item.id"
+                   size="large"  closable @on-close="handleClose">{{item.name}}</Tag>
+            </p>
+          </Card>
+          <Table
+            size="default"
+            no-data-text="没有可供选择的标签"
+            stripe border
+            :loading="loading2"
+            height="300"
+            :columns="columns2" :data="data2">
+          </Table>
+        </div>
+        <div slot="footer">
+          <Button size="large" @click="modal_create=false">取消</Button>
+          <Button size="large" type="primary" :loading="loading_create" @click="create">评价</Button>
+        </div>
+      </Modal>
+      <Modal v-model="modal_update" width="600" :closable="false">
+        <div>
+          <Card style="margin-bottom: 25px">
+            <p>
+              <span>您的评价：</span>
+              <Tag v-for="item in tagList"
+                   :key="item.id" :name="item.id"
+                   size="large"  closable @on-close="handleClose2">{{item.name}}</Tag>
+            </p>
+          </Card>
+          <Table
+            size="default"
+            no-data-text="没有可供选择的标签"
+            stripe border
+            :loading="loading3"
+            height="300"
+            :columns="columns3" :data="data3">
+          </Table>
+        </div>
+        <div slot="footer">
+          <Button size="large" @click="modal_update=false">取消</Button>
+          <Button size="large" type="primary" :loading="loading_update" @click="update">修改评价</Button>
+        </div>
+      </Modal>
+    </Row>
+  </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import expandRow from './table-expand'
+
+    export default {
+        name: 'historyManager',
+        components: { expandRow },
+        data() {
+            return{
+                loading: false,
+                roomName: '',
+                columns: [
+                    {
+                        type: 'expand',
+                        width: 50,
+                        render: (h, params) => {
+                            return h(expandRow, {
+                                props: {
+                                    row: params.row,
+                                }
+                            })
+                        }
+                    },
+                    {
+                        title: '大楼名称',
+                        key: 'building',
+                        align: 'center',
+                        width: 200,
+                        tooltip: true
+                    },
+                    {
+                        title: '房间号',
+                        key: 'conferenceName',
+                        align: 'center',
+                        width: 150,
+                        tooltip: true
+                    },
+                    {
+                        title: '申请单位',
+                        key: 'departmentName',
+                        align: 'center',
+                        tooltip: true
+                    },
+                    {
+                        title: '申请人',
+                        key: 'applicantName',
+                        align: 'center',
+                        width: 150,
+                        tooltip: true,
+                    },
+                    {
+                        title: '身份',
+                        key: 'identity',
+                        align: 'center',
+                        width: 150,
+                        tooltip: true,
+                    },
+                    {
+                        title: '使用时间',
+                        key: 'needdate',
+                        align: 'center',
+                        tooltip: true,
+                    },
+                    {
+                        title: '时间段',
+                        key: 'time',
+                        align: 'center',
+                        tooltip: true,
+                    },
+                    {
+                        title: '状态',
+                        align: 'center',
+                        tooltip: true,
+                        render: (h,params) => {
+                            if (this.data[params.index].progress == '被驳回') {
+                                return h('div', {}, [
+                                    h('span', {
+                                        style: {
+                                            color: 'red'
+                                        }
+                                    }, '已驳回')
+                                ])
+                            }else if(this.data[params.index].progress == '预约过期'){
+                                return h('div', {}, [
+                                    h('span', {
+                                        style: {
+                                            color: 'red'
+                                        }
+                                    }, '预约过期')
+                                ])
+                            }else if(this.data[params.index].progress == '通过审核' || this.data[params.index].progress == '已评价'){
+                                return h('div', {}, [
+                                    h('span', {
+                                        style: {
+                                            color: 'green'
+                                        }
+                                    }, this.data[params.index].progress)
+                                ])
+                            }else{
+                                return h('div', {}, [
+                                    h('span', {
+                                        style: {
+                                            color: 'black'
+                                        }
+                                    }, this.data[params.index].progress)
+                                ])
+                            }
+                        }
+                    }
+                ],
+                data: [],
+            }
+        },
+        mounted() {
+            this.init("初始化成功！", '');
+        },
+        methods: {
+            init(index,type){
+                this.data = []
+                axios({
+                    url: apiRoot + '/manager/departmentHistory',
+                    method: 'get'
+                }).then((res) => {
+                    if(res.data.code == 200){
+                        res.data.data.forEach((item) => {
+                            item.newParam = 'time'
+                            item.time = item.begintime + '-' + item.endtime
+                            if(type == ''){
+                                this.data.push(item)
+                            }else{
+                                if(item.conferenceName.indexOf(type) != -1){
+                                    this.data.push(item)
+                                }
+                            }
+                        })
+                        this.$Message.success(index);
+                    }else{
+                        this.$Message.error(res.data.message)
+                    }
+                }).catch((err) => {
+                    this.$Message.error("获取数据失败，请检查网络连接")
+                })
+            },
+            searchForApply() {
+                this.init("检索成功！", this.roomName)
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+  @import 'historyManager';
+</style>
---
+++ src/pages/manager/historyManager/table-expand.vue
@@ -0,0 +1,208 @@
+<style scoped>
+  .expand-row{
+    margin-bottom: 16px;
+  }
+  .hr_extend{
+    margin-top: 10px;
+    margin-bottom: 10px;
+    color: #d7dde4;
+  }
+</style>
+
+<template>
+  <Card style="font-size: 15px">
+    <Row style="padding-left: 30px; margin-bottom: 0px">预约详情：</Row>
+    <Divider size="small"/>
+    <Row>
+      <Col span="16" style="padding-right: 25px">
+        <p>会议参与者（默认第一个人为申请人）</p>
+        <Table
+          size="large"
+          no-data-text="数据异常"
+          stripe border
+          :loading="loading"
+          height="350"
+          :columns="columns" :data="data"></Table>
+      </Col>
+      <Col span="8">
+        <Row>
+          <Card>
+            <div>
+              <Row>
+                <Col span="5">
+                  申请理由：
+                </Col>
+                <Col span="19">
+                  <span v-if="reasonExist">{{this.info.requestreason}}</span>
+                  <span v-if="!reasonExist">未填写</span>
+                </Col>
+              </Row>
+            </div>
+          </Card>
+          <Card v-if="this.row.progress=='被驳回'" style="margin-top: 25px">
+            <div>
+              <Row>
+                <Col span="5">
+                  拒绝理由：
+                </Col>
+                <Col span="19">
+                  <span v-if="!info.refusereason==''">{{this.info.refusereason}}</span>
+                  <span v-if="info.refusereason==''">未填写</span>
+                </Col>
+              </Row>
+            </div>
+          </Card>
+          <Card v-if="this.row.progress=='已评价'" style="margin-top: 25px">
+            <div>
+              <Row>
+                <Col span="5">
+                  评价：
+                </Col>
+                <Col span="19">
+                  <Tag v-for="item in tagList" :key="item.tag"
+                       size="large" style="float: left">{{item.tag}}</Tag>
+                </Col>
+              </Row>
+            </div>
+          </Card>
+        </Row>
+      </Col>
+    </Row>
+  </Card>
+
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        props: {
+            row: Object
+        },
+        data(){
+            return {
+                info: {},
+                loading: false,
+                reasonExist: true,
+                tags: [],
+                tagList: [],
+                isEmpty: false,
+                columns: [
+                    {
+                        title: '姓名',
+                        key: 'name',
+                        align: 'center',
+                        tooltip: true
+                    },
+                    {
+                        title: '学/工号',
+                        key: 'userId',
+                        align: 'center',
+                        tooltip: true
+                    },
+                    {
+                        title: '部门',
+                        key: 'departmentName',
+                        align: 'center',
+                        tooltip: true
+                    },
+                    {
+                        title: '操作',
+                        key: 'operation',
+                        align: 'center',
+                        width: 150,
+                        render: (h, params) => {
+                            return h('Poptip', {
+                                props: {
+                                    placement: 'left-start',
+                                    width: '300',
+                                    wordWrap: 'true'
+                                }
+                            }, [
+                                h('Button', {
+                                    props: {
+                                        type: 'primary'
+                                    },
+                                    on:{
+                                        click: () => {
+                                            this.getTag(params.index);
+                                        }
+                                    }
+                                }, '查看历史评价'),
+                                h('div',{
+                                        slot: 'content'
+                                    }, this.tags.map((item) => {
+                                        return h('Tag', {
+                                            props: {
+                                                size: 'large'
+                                            },
+                                            style:{
+                                                float: 'left'
+                                            },
+                                            domProps: {
+                                                innerHTML: item
+                                            }
+                                        })
+                                    })
+                                )
+                            ])
+                        }
+                    }
+                ],
+                data: []
+            }
+        },
+        mounted(){
+            this.init()
+            console.log(this.row)
+        },
+        methods:{
+            init(){
+                this.loading = true
+                axios({
+                    url: apiRoot + '/user/appointmentDetail?id=' + this.row.id,
+                    method: 'get'
+                }).then((res) => {
+                    if(res.data.code == 200){
+                        this.info = res.data.data
+                        if(this.info.requestreason == '') this.reasonExist = false
+                        this.data.push(this.info.applicantName)
+                        if(this.info.participants.length != 0){
+                            this.info.participants.forEach((item) => {
+                                this.data.push(item)
+                            })
+                        }
+                        this.tagList = this.info.tagList
+
+                        this.loading = false;
+                    }else{
+                        this.$Message.error(res.data.message)
+                        this.loading = false;
+                    }
+                }).catch((err) => {
+                    console.log(err)
+                    this.$Message.error("获取详细信息失败，请检查网络连接！")
+                    this.loading = false;
+                })
+            },
+            getTag(index){
+                this.tags = [];
+                axios({
+                    url: apiRoot + '/user/usersTag?id=' + this.data[index].userId,
+                    method: 'get'
+                }).then((res) => {
+                    if(res.data.code == 200){
+                        let dat = res.data.data;
+                        dat.forEach((item)=> {
+                            this.tags.push(item.tag)
+                        })
+                    }else{
+                        this.$Message.error(res.data.message)
+                    }
+                }).catch((err) => {
+                    this.$Message.error("获取评价标签失败，请检查网络连接")
+                })
+            }
+        }
+    }
+</script>
